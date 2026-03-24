f1c287ad8ed953121972e322a79788061b6face7
Nandhakumar <nknandhakumar64@gmail.com>
2026-03-24 13:06:35 +0530

    Initial Nkart backend setup

---
+++ src/test/java/com/Nkart/NkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
