2390cf015fa5310cf4151f1b31b5e0ea9803d45d
Nandhakumar <nknandhakumar64@gmail.com>
2026-03-24 13:44:50 +0530

    update : constructs and getters and setters

--- src/main/java/com/Nkart/Entity/Products.java
+++ src/main/java/com/Nkart/Entity/Products.java
@@ -9,9 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Products {
 
     @Id
@@ -21,4 +18,60 @@ public class Products {
     private String description;
     private Double price;
     private String imageUrl;
+
+    // constructors
+
+    public Products() {
+
+    }
+
+    public Products(Long id, String name, String description, Double price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
