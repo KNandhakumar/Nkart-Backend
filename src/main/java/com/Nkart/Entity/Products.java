b282bf74422b5e882e424dc8d8fb271ccdf858b1
Nandhakumar <nknandhakumar64@gmail.com>
2026-03-24 13:19:01 +0530

    created product model

---
+++ src/main/java/com/Nkart/Entity/Products.java
@@ -0,0 +1,24 @@
+package com.Nkart.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imageUrl;
+}
